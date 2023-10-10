b50c58f6674a2c2b3f0840441c02e78b3148c704
Linas <linasmg58@gmail.com>
2023-10-10 16:02:28 +0200

    now in tests there is only testing

--- laboratorium01/test_pytest.py
+++ laboratorium01/test_pytest.py
@@ -3,14 +3,10 @@
 import skrypt01
 
 
-
-MOVES={'f':'Zwierzak idzie do przodu', 'b':'Zwierzak idzie do tyłu', 'l':'Zwierzak skręca w lewo', 'r':'Zwierzak skręca w prawo'}
-
-
 def test_display_works():
     assert skrypt01.display('1', False) == 0
 
 def test_run_list_only_moves():
-    assert skrypt01.run(['f', 'b', 'l', 'r', 'cos', 'q', '@', 'k'], MOVES) == [MOVES['f'], MOVES['b'], MOVES['l'], MOVES['r']] 
+    assert skrypt01.run(['f', 'b', 'l', 'r', 'cos', 'q', '@', 'k'],skrypt01.MOVES) == [skrypt01.MOVES['f'], skrypt01.MOVES['b'], skrypt01.MOVES['l'], skrypt01.MOVES['r']] 
 
 
--- laboratorium01/unitTest01.py
+++ laboratorium01/unitTest01.py
@@ -4,14 +4,11 @@ import unittest
 import skrypt01
 
 
-MOVES={'f':'Zwierzak idzie do przodu', 'b':'Zwierzak idzie do tyłu', 'l':'Zwierzak skręca w lewo', 'r':'Zwierzak skręca w prawo'}
-
-
 class Test_displayWorks(unittest.TestCase):
     def test_f_working(self):
-        self.assertEqual(skrypt01.run('f', MOVES), [MOVES['f']])
+        self.assertEqual(skrypt01.run('f', skrypt01.MOVES), [skrypt01.MOVES['f']])
     def test_other_than_fblr(self):
-        self.assertListEqual(skrypt01.run(['a', 's', 'w', '!', '@', 'p', 'o', 'k'], MOVES), [])
+        self.assertListEqual(skrypt01.run(['a', 's', 'w', '!', '@', 'p', 'o', 'k'], skrypt01.MOVES), [])
 
 
 if __name__ == '__main__':
